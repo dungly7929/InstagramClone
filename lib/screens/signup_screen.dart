f5648ffb18cbae024c9d72b3316aa7746e2940a9
BuildTools <unconfigured@null.spigotmc.org>
2022-07-28 16:13:04 -0400

    signup_screen GUI done

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -51,6 +51,28 @@ class _SignUpScreenState extends State<SignUpScreen> {
               height: 18,
             ),
             //Cicular Widget to accept and show our selected file
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1658786335136-0b3e83dcb63a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                ),
+                Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.add_a_photo,
+                      ),
+                    ))
+              ],
+            ),
+
+            const SizedBox(
+              height: 18,
+            ),
 
             //Textfield input for username
             TextFieldInput(
