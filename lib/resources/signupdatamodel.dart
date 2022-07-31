c111a076abf56be33af9a80c7372ef63bd2d620b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-31 15:21:14 -0400

    adding model and validator commit

---
+++ lib/resources/signupdatamodel.dart
@@ -0,0 +1,12 @@
+class SignUpDataModel {
+  String username;
+  String email;
+  String password;
+  String bio;
+
+  SignUpDataModel(
+      {required this.username,
+      required this.email,
+      required this.password,
+      required this.bio});
+}
--- lib/resources/validators.dart
+++ lib/resources/validators.dart
@@ -21,3 +21,38 @@ class Validatros {
     return "hehe";
   }
 }
+
+//valid Username input
+evalUsername(value) {
+  if (value.isEmpty) {
+    return "username is required";
+  } else {
+    if (value.length < 6) {
+      return "your username is too short !";
+    } else {
+      return null;
+    }
+  }
+}
+
+//validEmail input
+evalEmail(value) {
+  RegExp regex = RegExp(
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+  if (value.isEmpty) {
+    return "email is required";
+  } else if (!regex.hasMatch(value)) {
+    return 'please enter the valid email';
+  } else {
+    return null;
+  }
+}
+
+evalBio(value) {
+  if (value.isEmpty) {
+    return "Please enter your bio";
+  }
+  return null;
+}
+
+final validatorBloc = Validatros;
