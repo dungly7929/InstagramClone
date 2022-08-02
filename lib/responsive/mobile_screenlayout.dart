306ace9dbf5e2b9699f15520c3014a6cbaecedb7
BuildTools <unconfigured@null.spigotmc.org>
2022-08-02 16:41:03 -0400

    Testing userprovider function commit.

--- lib/responsive/mobile_screenlayout.dart
+++ lib/responsive/mobile_screenlayout.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:learningdart/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:learningdart/models/user.dart' as model;
 
 class MobileSreenLayout extends StatefulWidget {
   const MobileSreenLayout({Key? key}) : super(key: key);
@@ -12,9 +15,10 @@ class MobileSreenLayout extends StatefulWidget {
 class _MobileSreenLayoutState extends State<MobileSreenLayout> {
   @override
   Widget build(BuildContext context) {
+    model.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
       body: Center(
-        child: Text('This is mobile'),
+        child: Text(user.email),
       ),
     );
   }
